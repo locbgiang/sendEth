28bbd6a7ece78d2e78de150173ae7f09f79d27f7
unknown <locbgiang@gmail.com>
2022-01-29 21:43:28 -0800

    fix text

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,23 +8,23 @@ const Footer = () => {
                     <img src={logo} alt="logo" className='w-10' />
                 </div>
                 <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
-                <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
-                <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
-                <p className="text-white text-base text-center mx-2 cursor-pointer">Tutorials</p>
-                <p className="text-white text-base text-center mx-2 cursor-pointer">Wallets</p>
+                <a href='https://coinmarketcap.com' className="text-white text-base text-center mx-2 cursor-pointer">Market</a>
+                <a href='https://coinmarketcap.com/rankings/exchanges/' className="text-white text-base text-center mx-2 cursor-pointer">Exchange</a>
+                <a href='https://www.coinbase.com/learn/tips-and-tutorials' className="text-white text-base text-center mx-2 cursor-pointer">Tutorials</a>
+                <a href='https://metamask.io' className="text-white text-base text-center mx-2 cursor-pointer">Wallets</a>
                 </div>
             </div>
             <div className='flex justify-center items-center flex-col mt-5'>
                 <p className='text-white text-sm text-center'>
-                    Come join us
+                    EMAIL:
                 </p>
                 <p className='text-white text-sm text-center'>
-                    info@Kryptomastery.com
+                    Locbgiang@gmail.com
                 </p>
             </div>
             <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
             <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-                <p className="text-white text-left text-xs">@kryptomastery2022</p>
+                <p className="text-white text-left text-xs">Loc Giang</p>
                 <p className="text-white text-right text-xs">All rights reserved</p>
             </div>
         </div>
--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -3,7 +3,7 @@ import { BiSearchAlt } from 'react-icons/bi';
 import { RiHeart2Fill } from 'react-icons/ri';
 
 const ServiceCard = ({color, title, icon, subtitle}) => (
-    <div className='flex flex-row justfiy-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl'>
+    <div className='w-1/2 flex flex-row justfiy-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl'>
         <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
             {icon}
         </div>
@@ -19,10 +19,10 @@ const Services = () => {
         <div className="flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services">
             <div className='flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4'>
                 <div className='flex-1 flex flex-col justify-start items-start'>
-                    <h1 className='text-white text-3xl sm:text-5xl py-2 text-gradient'>
-                        Services that we 
+                    <h1 className='text-white text-3xl sm:text-5xl py-2'>
+                        Blockchain
                         <br />
-                        continue to improve
+                        Technology
                     </h1>
                 </div>
             </div>
@@ -31,19 +31,19 @@ const Services = () => {
                     color = "bg-[#2952E3]"
                     title = "Security Guaranteed"
                     icon = {<BsShieldFillCheck frontSize={21} className="text-white"/>}
-                    subtitle = "Security is guaranteed. We always maintain privacy and maintain the quality of our product."
+                    subtitle = "Transactions are secured by the Ethereum network."
                 />
                 <ServiceCard 
                     color = "bg-[#8945F8]"
-                    title = "Best exchange rates"
+                    title = "Trustless and Decentralized"
                     icon = {<BiSearchAlt frontSize={21} className="text-white"/>}
-                    subtitle = "Security is guaranteed. We always maintain privacy and maintain the quality of our product."
+                    subtitle = "Transactions do not require a centralized entity."
                 />
                 <ServiceCard 
                     color = "bg-[#F84550]"
-                    title = "Fastest transactions"
+                    title = "Fastest Transactions"
                     icon = {<RiHeart2Fill frontSize={21} className="text-white"/>}
-                    subtitle = "Security is guaranteed. We always maintain privacy and maintain the quality of our product."
+                    subtitle = "Move values in seconds from anywhere to anywhere."
                 />
             </div>
         </div>
